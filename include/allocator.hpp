0e5f3ae6a5537a9e90931f3935f69a5131c31e91
Matthias Killat <matthias.killat{at}gmx.de>
2020-06-19 09:51:59 +0200

    corrected acquisition and improved deleteion

--- include/allocator.hpp
+++ include/allocator.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <map>
 #include <mutex>
+#include <atomic>
 
 //simple allocator
 class DefaultAllocator
@@ -12,6 +13,7 @@ public:
     template <typename T, typename... Args>
     static T *allocate(Args &&... args)
     {
+        s_numAllocations.fetch_add(1);
         return new T(std::forward<Args>(args)...);
     }
 
@@ -19,12 +21,19 @@ public:
     static void free(T *p)
     {
         delete p;
+        s_numAllocations.fetch_sub(1);
     }
 
     static void print()
     {
+        std::cout << "DefaultAllocator allocations " << s_numAllocations << std::endl;
     }
+
+private:
+    static std::atomic<uint64_t> s_numAllocations;
 };
+
+std::atomic<uint64_t> DefaultAllocator::s_numAllocations{0};
 
 class MonitoredAllocator
 {
@@ -36,6 +45,7 @@ public:
         //std::cout << "allocated " << sizeof(T) << " bytes at " << p << std::endl;
         std::lock_guard<std::mutex> g(s_mutex);
         s_allocations[p] = sizeof(T);
+        s_numAllocations++;
         return p;
     }
 
@@ -48,8 +58,11 @@ public:
         if (iter != s_allocations.end())
         {
             delete p;
+
+            //very subtle race, if this is missing, we get double free errors...
             //std::cout << "deallocated " << sizeof(T) << " bytes at " << p << std::endl;
             s_allocations.erase(iter);
+            s_numAllocations--;
         }
         else
         {
@@ -60,13 +73,13 @@ public:
 
     static void print()
     {
-        std::cout << "Allocator free errors " << s_errors << std::endl;
-        std::cout << "Allocator current allocations" << std::endl;
         std::lock_guard<std::mutex> g(s_mutex);
-        for (auto &pair : s_allocations)
-        {
-            std::cout << pair.first << " " << pair.second << "bytes" << std::endl;
-        }
+        std::cout << "MonitoredAllocator free errors " << s_errors << std::endl;
+        std::cout << "MonitoredAllocator current allocations " << s_numAllocations << std::endl;
+        // for (auto &pair : s_allocations)
+        // {
+        //     std::cout << pair.first << " " << pair.second << "bytes" << std::endl;
+        // }
     }
 
     static size_t errors()
@@ -76,11 +89,13 @@ public:
 
     static std::mutex s_mutex; //todo: only for testing, must be removed together with the map to make it lock_free
     static std::map<void *, size_t> s_allocations;
+    static uint64_t s_numAllocations;
     static size_t s_errors;
 };
 
 std::mutex MonitoredAllocator::s_mutex;
 std::map<void *, size_t> MonitoredAllocator::s_allocations{};
+uint64_t MonitoredAllocator::s_numAllocations{0};
 size_t MonitoredAllocator::s_errors{0};
 
 //using Allocator = DefaultAllocator;
--- include/lockfree_wrapper.hpp
+++ include/lockfree_wrapper.hpp
@@ -6,6 +6,10 @@
 #include <set>
 #include <vector>
 #include <algorithm>
+#include <list>
+#include <string>
+
+#include "assert.h"
 
 //potentially useful for read often, write seldom lockfree structures
 //or prototyping where performance is not the primary issue (the internal copies are quite inefficient)
@@ -24,7 +28,10 @@ private:
     {
         FREE,
         USED,
-        RELEASED
+        RELEASED,
+        DELETABLE,
+        READY_TO_DELETE,
+        DELETED
     };
 
     struct HazardPointer
@@ -33,10 +40,33 @@ private:
         {
         }
 
+        void print()
+        {
+            std::cout << "HP " << id << " " << this << " ptr " << ptr.load() << " " << statusStr() << std::endl;
+        }
+
+        std::string statusStr()
+        {
+            switch (status)
+            {
+            case FREE:
+                return "FREE";
+            case USED:
+                return "USED";
+            case RELEASED:
+                return "RELEASED";
+            case DELETABLE:
+                return "DELETABLE";
+            case DELETED:
+                return "DELETED";
+            }
+            return "";
+        }
+
         std::atomic<T *> ptr{nullptr}; //the payload we want to protect todo: can be mase nonatomic in conjunctio with atomic status?
         HazardPointer *next{nullptr};
         std::atomic<uint32_t> status{FREE};
-        const uint64_t id; //unique and does not change (can use the this pointer instead later)
+        const uint64_t id; //unique and does not change (todo: not needed, can use the this pointer instead later)
     };
 
 public:
@@ -81,7 +111,7 @@ public:
         {
             if (!wrapper->updateObject(object, copy))
             {
-                wrapper->free(copy);
+                wrapper->deallocate(copy);
             }
 
             wrapper->releaseHazardPointer(*hp);
@@ -114,7 +144,7 @@ public:
     friend class TryWriteProxy<T>;
 
     template <typename... Args>
-    LockFree(Args &&... args) : currentObjectHazardPointer(createHazardPointer())
+    LockFree(Args &&... args) : currentObjectHazardPointer(new HazardPointer(0))
     {
         auto initialObject = allocate(std::forward<Args>(args)...);
         currentObjectHazardPointer->ptr.store(initialObject); //owned internally, only released in dtor, prevents deletion of current object
@@ -124,7 +154,7 @@ public:
 
     ~LockFree()
     {
-        std::cout << "Destructor" << std::endl;
+        std::cout << "Destructor #hazard pointers " << numHazardPointers.load() << std::endl;
         //should maybe also block acquisition (recycling existing ones) but this is bad since we would need to check it always
         // (acquisition will happen much more often compared to creation)
         canCreateHazardPointer.store(false, std::memory_order_release);
@@ -180,12 +210,17 @@ public:
 
     bool updateObject(T *expectedObject, T *newObject)
     {
+        //auto hp = acquireHazardPointer();
         //we cannot have an ABA problem here, ptr will be deleted and possibly recycled only after no one holds ptr anymore
         //in a hazardpointer (and therefore will not try to update with this old value)
         if (currentObjectHazardPointer->ptr.compare_exchange_strong(expectedObject, newObject))
         {
+            //assert(hp->ptr.load() == expectedObject);
+
+            //releaseHazardPointer(*hp);
             return true;
         }
+        //releaseHazardPointer(*hp);
         return false;
     }
 
@@ -216,26 +251,26 @@ public:
     template <typename Function, typename... Params>
     decltype(auto) invoke(Function &&f, Params &&... params)
     {
-        auto &hp = *acquireHazardPointer();
+        auto hp = acquireHazardPointer();
 
         do
         {
-            T *expected = ptr(hp);
+            T *expected = hp->ptr.load();
             T *copy = allocate(*expected); //local copy, protected against deletion by hp
 
             auto result = std::invoke(f, copy, std::forward<Params>(params)...);
 
             if (updateObject(expected, copy))
             {
-                releaseHazardPointer(hp);
+                releaseHazardPointer(*hp);
                 return result;
             }
 
             //our update failed, the copy is useless now
-            free(copy); //could optimize and use in place construction in memory instead
+            deallocate(copy); //could optimize and use in place construction in memory instead
 
             //we recycle the hazard pointer and load the new object state (can optimize by using load of compare exchange)
-            hp.ptr.store(currentObject());
+            hp->ptr.store(currentObject());
 
         } while (true);
     }
@@ -253,7 +288,7 @@ private:
     //this is also not the best structure to search in, there is potential for optimization
     //but it shows the general idea
 
-    std::atomic<uint64_t> numHazardPointers{0}; //i.e. list size
+    std::atomic<uint64_t> numHazardPointers{1}; //i.e. list size
     std::atomic<uint64_t> numUsedHazardPointers{1};
     std::atomic<uint64_t> numReleasedHazardPointers{0};
     std::atomic<HazardPointer *> hazardPointers{nullptr}; //managed hazard pointers, can be used to protect objects
@@ -272,8 +307,6 @@ private:
         //later we could control the number available hazardpointers like this
         //and hence the number of maximally needed copies
 
-        constexpr bool hazardPointerCreationIsImpossible = false;
-
         do
         {
             HazardPointer *hp = hazardPointers.load();
@@ -283,13 +316,23 @@ private:
                 uint32_t expectedStatus = FREE;
                 if (hp->status.compare_exchange_strong(expectedStatus, USED))
                 {
-                    hp->ptr = currentObject();
+                    auto ptr = currentObjectHazardPointer->ptr.load();
+                    do
+                    {
+                        hp->ptr.store(ptr);
+                        //is it still the same?
+                        if (currentObjectHazardPointer->ptr.compare_exchange_strong(ptr, ptr))
+                        {
+                            break;
+                        }
+                    } while (true);
+
                     numUsedHazardPointers.fetch_add(1);
                     return hp;
                 }
                 hp = hp->next;
             }
-        } while (hazardPointerCreationIsImpossible);
+        } while (!canCreateHazardPointer.load());
 
         //no free hazard pointer, create a new one
         auto hp = createHazardPointer();
@@ -301,7 +344,16 @@ private:
             hp->next = head;
             if (hazardPointers.compare_exchange_weak(head, hp))
             {
-                hp->ptr = currentObject();
+                auto ptr = currentObjectHazardPointer->ptr.load();
+                do
+                {
+                    hp->ptr.store(ptr);
+                    //is it still the same?
+                    if (currentObjectHazardPointer->ptr.compare_exchange_strong(ptr, ptr))
+                    {
+                        break;
+                    }
+                } while (true);
                 break;
             }
 
@@ -312,14 +364,15 @@ private:
     }
 
     //expect that it is a used hazard pointer
-    void releaseHazardPointer(HazardPointer &hp)
+    void
+    releaseHazardPointer(HazardPointer &hp)
     {
         //we are the only writer to this hazard pointer (hence no compare exchange needed)
         hp.status.store(RELEASED);
-        auto numReleased = numReleasedHazardPointers.fetch_add(1);
-        auto numUsed = numUsedHazardPointers.fetch_sub(1);
+        auto numReleased = numReleasedHazardPointers.fetch_add(1, std::memory_order_relaxed);
+        auto numUsed = numUsedHazardPointers.fetch_sub(1, std::memory_order_relaxed);
 
-        constexpr double deleteScanFactor = 0.25;
+        constexpr double deleteScanFactor = 0.3;
         if (numUsed * deleteScanFactor <= numReleased)
         {
             //when enough were released (relative to those used) we atomically reset the released counter and trigger a scan
@@ -334,8 +387,11 @@ private:
     {
         if (scanInProgress.test_and_set(std::memory_order_acq_rel))
         {
-            return false; //another scan is in progress, we skip the scan
+            //todo: we are not fully able to leave this out, there is some deletion race...why?
+            //return false; //another scan is in progress, we skip the scan
         }
+
+        //printHazards();
 
         //todo: we are lockfree, but the problem is: if the scanning thread dies, no one can ever scan again
         //and we leak memory (and probably fast) sinc eno hazard pointer will be freed
@@ -343,7 +399,7 @@ private:
         //so this must be prevented
         //major todo/question: can we do it in a robust lockfree way (without the scanInProgress flag)
 
-        std::set<HazardPointer *> deleteCandidates;
+        std::list<HazardPointer *> deleteCandidates;
         std::set<T *> usedPointers;
 
         {
@@ -353,36 +409,104 @@ private:
             while (hp)
             {
                 auto status = hp->status.load();
-                if (status == RELEASED)
+                if (status == RELEASED || status == DELETABLE /*|| status == READY_TO_DELETE*/)
                 {
-                    //hp->status.store(DELETABLE); //optimistic assumption: can delete later
-                    deleteCandidates.insert(hp);
+                    deleteCandidates.push_back(hp);
                 }
                 else if (status == USED) //note that it does not matter if the status became RELEASED inbetween (we just cannot free it in this scan)
                 {
                     usedPointers.insert(hp->ptr.load());
                 }
+                // else if (status == DELETED)
+                // {
+                //     hp->status.store(FREE);
+                // }
                 hp = hp->next;
             }
         }
 
-        std::set<T *> deleteSet;
+        std::list<HazardPointer *> deletableHazardPointers;
 
         //now check if we really can delete the candidates(i.e.no USED hazardpointer with the same ptr exists)
         for (auto hp : deleteCandidates)
         {
             auto ptr = hp->ptr.load();
-            if (usedPointers.find(ptr) == usedPointers.end())
+            if (hp->status.load() == RELEASED)
             {
-                deleteSet.insert(hp->ptr.load()); //avoid duplicates
+                if (usedPointers.find(ptr) == usedPointers.end())
+                {
+                    hp->status.store(DELETABLE);
+                    deletableHazardPointers.push_back(hp);
+                }
+            }
+            else
+            {
+                //READY_TO_DELETE or DELETABLE
+                deletableHazardPointers.push_back(hp);
+            }
+        }
+
+        //now the following holds: for any pointer ptr that is deletable there is a collection of hazard pointers
+        //which hold it, there also is a first in list holding ptr, call that hp0
+        //it cannot be that new hazardpointers with ptr are added in front of hp0, because ptr cannot be the current object(its hazardpointer is always used)
+        //and ptr cannot be recycled by the allocator before it is deleted
+
+        //therefore: all threads see the same hp0 for ptr and leave it but set all others to FREEs
+        //afterwards another pass trys a compare exchange on the DELETABLE ones (the hp0s),
+        //only the winner actually deletes the pointer, avoiding multiple deletion and leaks
+
+        //todo: can this be done in a less complicated/inefficient way without compromising lockfree robustness?
+
+        //printHazards();
+
+        std::set<T *> deleteSet;
+        std::list<HazardPointer *> deleteList;
+        for (auto hp : deletableHazardPointers)
+        {
+            auto ptr = hp->ptr.load();
+            if (deleteSet.find(ptr) == deleteSet.end())
+            {
+                //first occurence, delete later
+                deleteSet.insert(ptr);
+                hp->status.store(READY_TO_DELETE);
+                deleteList.push_back(hp);
+            }
+            else
+            {
+                //further occurences, can be marked as free
+                //hp->ptr.store(nullptr); //should not be required
                 hp->status.store(FREE);
             }
         }
 
-        for (auto p : deleteSet)
+        //printHazards();
+
+        for (auto hp : deleteList)
         {
-            free(p);
+            uint32_t expected = READY_TO_DELETE;
+            do
+            {
+                auto ptr = hp->ptr.load();
+                //cannot go to FREE directly, ptr may be used again before it is deleted (causing us to delete the wrong pointer)
+                if (hp->status.compare_exchange_strong(expected, DELETED))
+                {
+                    //if we stall here, there is a leak of the hazard pointer and its resource...hard to avoid
+                    while (ptr != nullptr)
+                    {
+                        if (hp->ptr.compare_exchange_strong(ptr, nullptr))
+                        {
+                            hp->status.store(FREE);
+                            deallocate(ptr); //only one thread can win and delete the pointer
+                            break;
+                        }
+                    }
+                    break;
+                }
+            } while (expected == READY_TO_DELETE); //if not, someone else deleted, ok
         }
+
+        // std::cout << "scan complete" << std::endl;
+        // printHazards();
 
         scanInProgress.clear(std::memory_order_acq_rel);
         return true;
@@ -394,7 +518,7 @@ private:
         return Allocator::allocate<T>(std::forward<Args>(args)...);
     }
 
-    void free(T *p)
+    void deallocate(T *p)
     {
         Allocator::free(p);
     }
@@ -403,9 +527,27 @@ private:
     {
         if (canCreateHazardPointer.load())
         {
+            constexpr uint32_t MAX_HAZARDS{1000}; //todo: max limit mechanism works not exact right now, but not that important for now
+            auto id = numHazardPointers.fetch_add(1);
+            if (id > MAX_HAZARDS)
+            {
+                canCreateHazardPointer.store(false); //created last one
+            }
             //can use a pool later
-            return new HazardPointer(numHazardPointers.fetch_add(1));
+            return new HazardPointer(id);
         }
         return nullptr;
+    }
+
+    void printHazards()
+    {
+        std::cout << "****************" << std::endl;
+        auto hp = hazardPointers.load();
+        while (hp)
+        {
+            hp->print();
+            hp = hp->next;
+        }
+        std::cout << "****************" << std::endl;
     }
 };
